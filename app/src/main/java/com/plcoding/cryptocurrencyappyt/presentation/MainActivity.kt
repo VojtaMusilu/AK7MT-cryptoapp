6a7c65c1102253aeb92af09d475e39bb44de0007
VojtaMusilu <88598423+VojtaMusilu@users.noreply.github.com>
2023-01-31 23:15:03 +0100

    add splashscreen

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/MainActivity.kt
@@ -3,11 +3,14 @@ package com.plcoding.cryptocurrencyappyt.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.plcoding.cryptocurrencyappyt.MainVIewModel
 import com.plcoding.cryptocurrencyappyt.presentation.coin_detail.CoinDetailScreen
 import com.plcoding.cryptocurrencyappyt.presentation.coin_list.CoinListScreen
 import com.plcoding.cryptocurrencyappyt.presentation.theme.CryptocurrencyAppYTTheme
@@ -15,8 +18,16 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainVIewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.isLoading.value
+            }
+        }
         setContent {
             CryptocurrencyAppYTTheme {
                 Surface(color = MaterialTheme.colors.background) {
